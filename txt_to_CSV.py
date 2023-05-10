4ab02cfc30b4db3fe7d208f6a73d74d55e16cb85
NICHOLAS EKENEDILICHUKWU ENEH <127051297+Nickjet101@users.noreply.github.com>
2023-05-10 22:45:01 +0100

    txt_to_CSV.py

--- txt_to_CSV.py
+++ txt_to_CSV.py
@@ -1,17 +1,27 @@
 import csv
+import os
+import sys
 
-with open('Phone_Number_list.txt', 'r') as file:
-    phone_numbers = file.read().splitlines()
+# Open the text file and read its content
+with open(os.path.join(sys.path[0], 'Phone_Number_list.txt'), 'r') as file:
+    text = file.read()
 
-with open('phone_numbers.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Phone Number Lists"])
-    writer.writerow(phone_numbers)
-    if writer:
-         print(" ")
-         print('Phone_numbers.txt file has been successfully converted to CSV File.')
-         print("")
-    else: 
-         print('Phone_numbers.txt file has not been successfully converted to CSV File.')
+# Split the text into rows in a list using a delimiter
+rows = text.split()
+
+# Open a new CSV file and write the rows to it
+with open(os.path.join(sys.path[0],'Phone_Number_list.csv'), 'w', newline= '') as file:
+    write_csv = csv.writer(file)
+    write_csv.writerow(['Phone Number List:'])
+    for row in rows:
+        each_row = row.split()
+        write_csv.writerow(each_row)
+
+if write_csv:
+    print(" ")
+    print('The text file has been converted to the csv file, Phone_Number_list.csv, successfully.')
+    print(" ")
+else:
+    print('The text file is not converted to csv file')
 
         
